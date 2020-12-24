cd38dd46150cde19de5a27b94e436b8bf6e31536
VitaliiKlimov <alex-ustasa@yandex.ru>
2020-12-24 21:55:04 +1000

    Add spring security

---
+++ src/main/java/com/votingsys/AuthorizedUser.java
@@ -0,0 +1,33 @@
+package com.votingsys;
+
+import com.votingsys.model.User;
+import com.votingsys.to.UserTo;
+import com.votingsys.util.UserUtil;
+
+public class AuthorizedUser extends org.springframework.security.core.userdetails.User {
+    private static final long serialVersionUID = 1L;
+
+    private UserTo userTo;
+
+    public AuthorizedUser(User user) {
+        super(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, user.getRoles());
+        this.userTo = UserUtil.asTo(user);
+    }
+
+    public int getId() {
+        return userTo.id();
+    }
+
+    public void update(UserTo newTo) {
+        userTo = newTo;
+    }
+
+    public UserTo getUserTo() {
+        return userTo;
+    }
+
+    @Override
+    public String toString() {
+        return userTo.toString();
+    }
+}
